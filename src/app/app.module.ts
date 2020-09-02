2785e70a99edda227528dd11279924fed6b02ad7
zipit145 <gregbforney@gmail.com>
2020-09-02 13:27:34 -0600

    added angular material, created angular material module (probably will be refactored into component specific modules, not sute yet), added control component with example buttons and some basic layout/styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BoardComponent } from './board/board.component';
 import { CharacterDashboardComponent } from './character-dashboard/character-dashboard.component';
+import { ControlsComponent } from './controls/controls.component';
+
+import { AngularMaterialModule } from './angular-material.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
-    CharacterDashboardComponent
+    CharacterDashboardComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
---
+++ src/app/controls/controls.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularMaterialModule } from '../angular-material.module'
+
+@Component({
+  selector: 'app-controls',
+  templateUrl: './controls.component.html',
+  styleUrls: ['./controls.component.css']
+})
+export class ControlsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
