edc88e0db9db18b4ea218889dc23916e42b05834
zipit145 <gregbforney@gmail.com>
2020-09-03 12:56:39 -0600

    added some basic sprites, floor, wall, stairs to the board

--- src/app/board.ts
+++ src/app/board.ts
@@ -1,5 +1,6 @@
 export interface Board {
     id: number;
     name: string;
+    type: string;
     piece: {};
 }
--- src/app/mock-board.ts
+++ src/app/mock-board.ts
@@ -4,501 +4,601 @@ export const BOARD: Board[] = [
     {
         id: 1,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 2,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 3,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 4,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 5,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 6,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 7,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 8,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 9,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 10,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 11,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 12,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 13,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 14,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 15,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 16,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 17,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 18,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 19,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 20,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 21,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 22,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 23,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 24,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 25,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 26,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 27,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 28,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 29,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 30,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 31,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 32,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 33,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 34,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 35,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 36,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 37,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 38,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 39,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 40,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 41,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 42,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 43,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 44,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 45,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 46,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 47,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 48,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 49,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 50,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 51,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 52,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 53,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 54,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 55,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 56,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 57,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 58,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 59,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 60,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 61,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 62,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 63,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 64,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 65,
         name: "first space",
+        type: "stairs",
         piece: {}
     },
     {
         id: 66,
         name: "first space",
+        type: "stairs",
         piece: {}
     },
     {
         id: 67,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 68,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 69,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 70,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 71,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 72,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 73,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 74,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 75,
         name: "first space",
+        type: "stairs",
         piece: {}
     },
     {
         id: 76,
         name: "first space",
+        type: "stairs",
         piece: {}
     },
     {
         id: 77,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 78,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 79,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 80,
         name: "first space",
+        type: "brick",
         piece: {}
     },
     {
         id: 81,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 82,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 83,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 84,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 85,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 86,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 87,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 88,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 89,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 90,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 91,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 92,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 93,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 94,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 95,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 96,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 97,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 98,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 99,
         name: "first space",
+        type: "floor",
         piece: {}
     },
     {
         id: 100,
         name: "first space",
+        type: "floor",
         piece: {}
     },
 
